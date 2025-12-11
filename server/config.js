cd003927ce6c73e4f38517b3ed57b546c2ec6d89
zyrafaq <krzesimir@hobler.pl>
2025-12-11 20:25:28 +0100

    Fix logical condition in defineLevelSkillPoints function

--- server/config.js
+++ server/config.js
@@ -103,7 +103,7 @@ module.exports = {
     defineLevelSkillPoints: level => {
         if (level < 2) return 0;
         if (level <= 40) return 1;
-        if (level <= 45 && level & 1 === 1) return 1;
+        if (level <= 45 && level && 1 === 1) return 1;
         return 0;
     },
 
